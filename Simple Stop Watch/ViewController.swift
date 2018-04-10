bfdcc7eb52c5d1f52a76f8d247beb11ae238e458
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-10 15:42:13 +0900

    stop watch

--- Simple Stop Watch/ViewController.swift
+++ Simple Stop Watch/ViewController.swift
@@ -16,9 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in
-            self.updateTime()
-        })
+        //myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()})
     }
 
     func updateTime() {
@@ -28,6 +26,18 @@ class ViewController: UIViewController {
         let msec = count - (min * 60 * 100) - (sec * 100)
         timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
     }
-
+    
+    @IBAction func start(_ sender: Any) {
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()}) // updateTime을 실행시킵니다.
+        
+    }
+    @IBAction func stop(_ sender: Any) {
+        myTimer.invalidate() // 스탑워치를 중지 시킵니다.
+    }
+    @IBAction func reset(_ sender: Any) {
+        myTimer.invalidate() // 스탑워치를 중지 시키고
+        timeLabel.text = "00:00:00" // 레이블을 초기화 시킵니다.
+    }
+    
 }
 
